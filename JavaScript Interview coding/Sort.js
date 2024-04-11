dac5da62d2fbc5890afe295bb6b57cc5c82c1e15
NehAlok <nehalok@192.168.1.4>
2024-04-11 19:22:19 +0530

    cleanup code

--- JavaScript Interview coding/Sort.js
+++
@@ -1,8 +0,0 @@
-//sort array in descending order;
-
-const arr = [1, 23, -43, 67, 98];
-const strSort = ["ALok", "Neha", "Pankaj", "Latun", "Zebra"];
-const res = arr.sort().reverse();
-console.log(res);
-const result = strSort.sort();
-console.log(result);
